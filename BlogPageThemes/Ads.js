9ec484ec04dde652d366f31bab41e262aecb549e
PrinceMakavana <princemakavana61@gmail.com>
2023-01-09 11:08:11 +0530

    js update

---
+++ BlogPageThemes/Ads.js
@@ -0,0 +1,52 @@
+window.googletag = window.googletag || { cmd: [] };
+googletag.cmd.push(function () {
+    googletag.defineSlot(&#39; /22857973068/Bottom_Blog &#39;, [[970, 250], [980, 90], [728, 90]], &#39; div - gpt - ad - 1673241287052 - 0 &#39;).addService(googletag.pubads());
+googletag.pubads().enableSingleRequest();
+googletag.enableServices();
+  });
+
+window.googletag = window.googletag || { cmd: [] };
+googletag.cmd.push(function () {
+    googletag.defineSlot(&#39; /22857973068/Top - of - Blog &#39;, [[768, 1024], [980, 120], [480, 320], [320, 50], [970, 90], [1024, 768]], &#39; div - gpt - ad - 1671039052633 - 0 &#39;).addService(googletag.pubads());
+googletag.pubads().enableSingleRequest();
+googletag.enableServices();
+  });
+
+window.googletag = window.googletag || { cmd: [] };
+googletag.cmd.push(function () {
+    googletag.defineSlot(&#39; /22857973068/Head_horizontal_ad_2 &#39;, [[120, 30], [88, 31], [120, 60], &#39; fluid &#39;, [120, 600], [120, 20], [120, 90]], &#39; div - gpt - ad - 1671338834683 - 0 &#39;).addService(googletag.pubads());
+googletag.pubads().enableSingleRequest();
+googletag.enableServices();
+  });
+
+
+{/* <script async='async' src='https://securepubads.g.doubleclick.net/tag/js/gpt.js'/>
+<script>
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot(&#39;/22857973068/Bottom_Blog&#39;, [[970, 250], [980, 90], [728, 90]], &#39;div-gpt-ad-1673241287052-0&#39;).addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+</script>
+   <script async='async' src='https://securepubads.g.doubleclick.net/tag/js/gpt.js'/>
+<script>
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot(&#39;/22857973068/Top-of-Blog&#39;, [[768, 1024], [980, 120], [480, 320], [320, 50], [970, 90], [1024, 768]], &#39;div-gpt-ad-1671039052633-0&#39;).addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+</script>
+    
+    <script async='async' src='https://securepubads.g.doubleclick.net/tag/js/gpt.js'/>
+<script>
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot(&#39;/22857973068/Head_horizontal_ad_2&#39;, [[120, 30], [88, 31], [120, 60], &#39;fluid&#39;, [120, 600], [120, 20], [120, 90]], &#39;div-gpt-ad-1671338834683-0&#39;).addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+</script>
+    <!-- Google Ad Manager Over-->
+     */}
---
+++ BlogPageThemes/securePage.js
@@ -0,0 +1,55 @@
+// !function () {
+//   function detectDevTool(allow) {
+//     if (isNaN(+allow)) allow = 100;
+//     var start = +new Date(); // Validation of built-in Object tamper prevention.
+//     debugger;
+//     // alert("isable debugger");
+//     var end = +new Date(); // Validates too.
+//     if (isNaN(start) || isNaN(end) || end - start > allow) {
+//       // input your code here when devtools detected.
+
+//       document.getElementsByTagName("BODY")[0].innerHTML = "<h1>Disable debugger</h1>";
+//       alert("Disable debugger");
+//     }
+//   }
+//   if (window.attachEvent) {
+//     if (document.readyState === "complete" || document.readyState === "interactive") {
+//       detectDevTool();
+//       window.attachEvent('onresize', detectDevTool);
+//       window.attachEvent('onmousemove', detectDevTool);
+//       window.attachEvent('onfocus', detectDevTool);
+//       window.attachEvent('onblur', detectDevTool);
+//     } else {
+//       setTimeout(argument.callee, 0);
+//     }
+//   } else {
+//     window.addEventListener('load', detectDevTool);
+//     window.addEventListener('resize', detectDevTool);
+//     window.addEventListener('mousemove', detectDevTool);
+//     window.addEventListener('focus', detectDevTool);
+//     window.addEventListener('blur', detectDevTool);
+//   }
+// }();
+
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+  }
+  );
+  document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+      return false;
+    }
+  }
+  
